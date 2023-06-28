289da34d29c7ba811b0357859bf772fa396cb390
Tie_Guo <117576846+Tie-Guo@users.noreply.github.com>
2023-06-28 20:20:38 +0800

    Add files via upload

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -47,11 +47,12 @@ class FPS extends TextField
 		currentFPS = 0;
 		selectable = false;
 		mouseEnabled = false;
-		defaultTextFormat = new TextFormat("_sans", 14, color);
+		defaultTextFormat = new TextFormat(Paths.font("vcr.ttf"), 12, color);
 		autoSize = LEFT;
 		multiline = true;
 		text = "FPS: ";
-
+		textColor = 0xFFFFFFFF;
+ 
 		cacheCount = 0;
 		currentTime = 0;
 		times = [];
@@ -61,14 +62,49 @@ class FPS extends TextField
 		{
 			var time = Lib.getTimer();
 			__enterFrame(time - currentTime);
+		
 		});
 		#end
 	}
+	
+	public static var currentColor = 0;	
+	var skippedFrames = 0;
+
+	var ColorArray:Array<Int> = [
+		0x9400D3,
+		0x4B0082,
+		0x0000FF,
+		0x00FF00,
+		0xFFFF00,
+		0xFF7F00,
+		0xFF0000
+		];
 
 	// Event Handlers
 	@:noCompletion
 	private #if !flash override #end function __enterFrame(deltaTime:Float):Void
 	{
+	
+		if (ClientPrefs.rainbowFPS)
+		{
+			if (skippedFrames >= 6)
+			{
+				if (currentColor >= ColorArray.length)
+					currentColor = 0;
+				textColor = ColorArray[currentColor];
+				currentColor++;
+				skippedFrames = 0;
+			}
+			else
+			{
+				skippedFrames++;
+			}
+		}
+		else
+		{
+			textColor = 0xFFFFFFFF;
+		}
+		
 		currentTime += deltaTime;
 		times.push(currentTime);
 
@@ -89,12 +125,18 @@ class FPS extends TextField
 			#if openfl
 			memoryMegas = Math.abs(FlxMath.roundDecimal(System.totalMemory / 1000000, 1));
 			text += "\nMemory: " + memoryMegas + " MB";
+			text += "\nTG Engine v" + MainMenuState.tgEngineVersion;
 			#end
-
-			textColor = 0xFFFFFFFF;
-			if (memoryMegas > 3000 || currentFPS <= ClientPrefs.framerate / 2)
+			
+			var newmemoryMegas:Float = 0;
+			// textColor = 0xFFFFFFFF;
+			if (memoryMegas > 1000) // || currentFPS <= ClientPrefs.framerate / 2)
 			{
-				textColor = 0xFFFF0000;
+			newmemoryMegas = Math.ceil( Math.abs( System.totalMemory ) / 10000000 / 1.024)/100;
+				// textColor = 0xFFFFFFFF;
+				text = "FPS: " + currentFPS;
+				text += "\nMemory: " + newmemoryMegas + " GB";
+				text += "\nTG Engine v" + MainMenuState.tgEngineVersion;
 			}
 
 			#if (gl_stats && !disable_cffi && (!html5 || !canvas))
@@ -108,4 +150,8 @@ class FPS extends TextField
 
 		cacheCount = currentCount;
 	}
+	
+	
+
+	
 }
