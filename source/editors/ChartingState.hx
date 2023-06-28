c101e51f56bcfdd87e08f0e63fac0bb8192bfb6b
Tie_Guo <117576846+Tie-Guo@users.noreply.github.com>
2023-06-28 10:39:24 +0800

    Add files via upload

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -3093,7 +3093,7 @@ class ChartingState extends MusicBeatState
 			if (CoolUtil.difficulties[PlayState.storyDifficulty].toLowerCase() != 'normal') {
 				SUtil.saveContent(Paths.formatToSongPath(_song.song) + postfix + '-' + CoolUtil.difficulties[PlayState.storyDifficulty].toLowerCase(), ".json", data.trim(), 'chart');
 			} else {
-				SUtil.saveContent(Paths.formatToSongPath(_song.song) + postfix, + ".json", data.trim(), 'chart');
+				SUtil.saveContent(Paths.formatToSongPath(_song.song) + postfix, ".json", data.trim(), 'chart');
 			}
 			#else
 			_file = new FileReference();
