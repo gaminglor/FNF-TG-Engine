1a12a0a2154bc62070a25580065725827052cb31
Tie_Guo <117576846+Tie-Guo@users.noreply.github.com>
2023-07-27 18:24:15 +0800

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -76,9 +76,9 @@ class MainMenuState extends MusicBeatState
 			if(FileSystem.exists(Paths.mods(Paths.currentModDirectory + '/' + 'images/mainmenu/list.txt')) || FileSystem.exists(Paths.mods('images/mainmenu/list.txt')))
 				optionShit = CoolUtil.coolTextFile(Paths.modFolders('images/mainmenu/list.txt'));
 			else
-				optionShit = CoolUtil.coolTextFile(SUtil.getPath() + Paths.getPath('images/mainmenu/list.txt'), TEXT, library);
+				optionShit = CoolUtil.coolTextFile(SUtil.getPath() + Paths.getPath('images/mainmenu/list.txt'), TEXT);
 		#else
-			optionShit = CoolUtil.coolTextFile(SUtil.getPath() + Paths.getPath('images/mainmenu/list.txt'), TEXT, library);
+			optionShit = CoolUtil.coolTextFile(SUtil.getPath() + Paths.getPath('images/mainmenu/list.txt'), TEXT);
 		#end
 
 		camGame = new FlxCamera();
